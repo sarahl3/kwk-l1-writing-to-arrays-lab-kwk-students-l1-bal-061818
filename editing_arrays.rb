049337f43c8cf0cd26ae989e71a709ce9c7f4744
Sarah Li <sarcl@hotmail.com>
2018-06-20 15:44:43 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -15,4 +15,5 @@ end
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
   rainbow_colors.push("green")
+  rainbow_colors.push("blue")
 end
